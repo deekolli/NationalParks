6cdcddd8801785426cf546a7796684007fbfb480
kdeep <kdeep@DESKTOP-TT13425>
2021-07-15 21:29:17 -0400

    Add project files.

---
+++ NationalParks/APIHandlerManager/APIHandler.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using Newtonsoft.Json;
+using NationalParks.Models;
+
+namespace NationalParks.APIHandlerManager
+{
+  public class APIHandler
+  {
+    // Obtaining the API key is easy. The same key should be usable across the entire
+    // data.gov developer network, i.e. all data sources on data.gov.
+    // 
+
+    static string BASE_URL = "https://api.data.gov/ed/collegescorecard/v1/schools?per_page=4";
+    static string API_KEY = "xeOUK1hsD4oethe5kTM45q37JI8YxaUzfx88B3DN"; //Add your API key here inside ""
+
+    HttpClient httpClient;
+
+    /// <summary>
+    ///  Constructor to initialize the connection to the data source
+    /// </summary>
+    public APIHandler()
+    {
+      httpClient = new HttpClient();
+      httpClient.DefaultRequestHeaders.Accept.Clear();
+      httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+      httpClient.DefaultRequestHeaders.Accept.Add(
+          new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+    }
+
+        /// <summary>
+        /// Method to receive data from API end point as a collection of objects
+        /// 
+        /// JsonConvert parses the JSON string into classes
+        /// </summary>
+        /// <returns></returns>
+        public string replaceString(string responseString)
+        {
+            string responseStringModified = responseString;
+            responseStringModified = responseStringModified.Replace("school.name", "schoolname");
+            responseStringModified = responseStringModified.Replace("school.city", "schoolcity");
+            responseStringModified = responseStringModified.Replace("school.state", "schoolstate");
+
+
+            return responseStringModified;
+        }
+        public Parks GetParks()
+    {
+      string NATIONAL_PARK_API_PATH = BASE_URL + "&_fields=school.name,school.city,school.state,id";
+      string parksData = "";
+
+      Parks parks = null;
+
+      httpClient.BaseAddress = new Uri(NATIONAL_PARK_API_PATH);
+
+      // It can take a few requests to get back a prompt response, if the API has not received
+      //  calls in the recent past and the server has put the service on hibernation
+      try
+      {
+        HttpResponseMessage response = httpClient.GetAsync(NATIONAL_PARK_API_PATH).GetAwaiter().GetResult();
+        if (response.IsSuccessStatusCode)
+        {
+          parksData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                    parksData = replaceString(parksData);
+                }
+
+        if (!parksData.Equals(""))
+        {
+          // JsonConvert is part of the NewtonSoft.Json Nuget package
+          parks = JsonConvert.DeserializeObject<Parks>(parksData);
+        }
+      }
+      catch (Exception e)
+      {
+        // This is a useful place to insert a breakpoint and observe the error message
+        Console.WriteLine(e.Message);
+      }
+
+      return parks;
+    }
+  }
+}
---
+++ NationalParks/Models/Parks.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NationalParks.Models
+{
+    // Model for parks data. These classes can be obtained by either using 
+    // the Visual Studio editor (right-click -> Paste Special -> Paste Json as Classes)
+    // or sites such as JsonToCSHarp
+
+
+
+    public class Rootobject
+    {
+        public Metadata metadata { get; set; }
+        public Result[] results { get; set; }
+    }
+
+    public class Metadata
+    {
+        public int page { get; set; }
+        public int total { get; set; }
+        public int per_page { get; set; }
+    }
+
+    public class Result
+    {
+        public LatestProgramsCip_4_Digit[] latestprogramscip_4_digit { get; set; }
+    }
+
+    public class LatestProgramsCip_4_Digit
+    {
+        public string code { get; set; }
+        public string title { get; set; }
+        public string ope6_id { get; set; }
+        public int unit_id { get; set; }
+        public School school { get; set; }
+        public Credential credential { get; set; }
+       
+    }
+
+    public class School
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+        public int main_campus { get; set; }
+    }
+
+    public class Credential
+    {
+        public int level { get; set; }
+        public string title { get; set; }
+    }
+
+   
+
+   
+
+    
+
+
+
+
+   
+
+
+   
+
+
+
+
+
+
+    public class Parks
+    {
+        public Metadata metadata { get; set; }
+        public List<Park> results { get; set; }
+    }
+
+
+
+   
+
+    public class Park
+    {
+        public string schoolname { get; set; }
+        public string schoolcity { get; set; }
+        public string schoolstate { get; set; }
+        public int id { get; set; }
+    }
+
+
+}
